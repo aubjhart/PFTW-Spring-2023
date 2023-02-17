33515c0124bc3c880a80b6877de62a900ce29cab
Aubrey <ajh@aubreyjhart.com>
2023-02-16 21:30:05 -0600

    week 5 5.2

---
+++ week_5/exercises/5.2/bubble.js
@@ -0,0 +1,31 @@
+let square1;
+
+function setup() {
+  createCanvas(600, 400);
+  square1 = new Square();
+}
+
+function draw() {
+  background('cyan');
+  square1.move();
+  square1.show();
+}
+
+class Square {
+  constructor(x, y) {
+    this.x = 300;
+    this.y = 200;
+  }
+
+  show() {
+    strokeWeight(5);
+    stroke('magenta');
+    noFill();
+    rect(this.x, this.y, 100, 100);
+  }
+
+  move() {
+    this.x = this.x + random(-3,3);
+    this.y = this.y + random(-3,3);
+  }
+}
